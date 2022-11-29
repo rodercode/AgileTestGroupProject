57f5a7c4046e3b0a62204284f7c2927fbdaf14a3
MasadanThe <oskar.nylund@gmail.com>
2022-11-29 19:05:07 +0100

    Added black screen at 1280x720 resolution

--- src/main/java/com/example/agiletestgroupproject/Snake.java
+++ src/main/java/com/example/agiletestgroupproject/Snake.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,15 @@ import java.io.IOException;
 public class Snake extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Group groupOfNodes = new Group();;
+        // Add all the objects to draw to this group
+        Group groupOfNodes = new Group();
+
+        // Scene at 1280x720 pixels
+        Scene scene = new Scene(groupOfNodes, 1280, 720);
+        scene.setFill(Color.BLACK);
+        stage.setTitle("Snake!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
