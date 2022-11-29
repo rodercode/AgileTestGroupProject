5eafde1ac15b85ee29e9476ce6ad0c8fe634b5ab
MasadanThe <oskar.nylund@gmail.com>
2022-11-29 19:02:53 +0100

    Renamed 'HelloApplication' to Snake and added group

--- src/main/java/com/example/agiletestgroupproject/Snake.java
+++ src/main/java/com/example/agiletestgroupproject/Snake.java
@@ -2,19 +2,16 @@ package com.example.agiletestgroupproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Snake extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        Group groupOfNodes = new Group();;
     }
 
     public static void main(String[] args) {
