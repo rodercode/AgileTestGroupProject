d623f629817da7a3a494994a4a36073de6eecf3a
RoderCode <Rodersnow@gmail.com>
2022-12-01 13:48:27 +0100

    add gets and sets in SnakeBody

--- src/main/java/com/example/agiletestgroupproject/SnakeBody.java
+++ src/main/java/com/example/agiletestgroupproject/SnakeBody.java
@@ -16,4 +16,24 @@ public class SnakeBody {
     public SnakeBody() {
 
     }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
 }
